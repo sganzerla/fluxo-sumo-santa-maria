1bd91c7d915ac71d9db8be6562620fea7f586c8b
alisson.sganzerla <alisson.sganzerla@gmailcom>
2021-11-05 10:04:14 -0300

    chore: criando passageiros para as paradas

---
+++ rota-1_bombeiros-ufsm/files/run.ps1
@@ -0,0 +1,4 @@
+
+# python.exe "C:\Program Files (x86)\Eclipse\Sumo\tools\randomTrips.py" -n .\osm.net.xml -e 500000 -o .\osm.passenger.routes.xml --persontrips --trip-attributes modes=public
+
+python .\runner.py
