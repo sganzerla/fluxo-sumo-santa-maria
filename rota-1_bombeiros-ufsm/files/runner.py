fd52b61aa595341a1e9637b4681fc0ea5b3bd5f3
alisson.sganzerla <alisson.sganzerla@gmailcom>
2021-11-11 23:05:16 -0300

    chore: organizando busstopsPersons print

--- rota-1_bombeiros-ufsm/files/runner.py
+++ rota-1_bombeiros-ufsm/files/runner.py
@@ -16,6 +16,7 @@ from sumolib import checkBinary  # noqa
 
 import traci
 
+
 def get_options():
     optParser = optparse.OptionParser()
     optParser.add_option("--nogui", action="store_true",
@@ -23,40 +24,47 @@ def get_options():
     options, args = optParser.parse_args()
     return options
 
+
 def orderBusStopByName(busstops):
     row = len(busstops)
     col = 2
     orderBusStops = [[0 for j in range(col)] for i in range(row)]
-    
-    i=0
+
+    i = 0
     for bus in busstops:
         orderBusStops[i][0] = bus
         orderBusStops[i][1] = traci.busstop.getName(bus).split("p")[1]
-        i+=1
-    
-    # for b in orderBusStops:
-    #     print(b[0],b[1])
+        i += 1
+
     # ordenar as paradas pelo nome
     orderBusStops.sort(key=lambda x: (int(x[1]), int(x[0])))
-  
-    # for b in orderBusStops:
-    #     print(b[0],b[1])
+
+    
     return orderBusStops
 
-def getBus(step):
+
+def print_bus_persons(step):
     allBus = traci.vehicle.getIDList()
     txt = "step: " + str(step)
     for bus in allBus:
         if traci.vehicle.getTypeID(bus) == "bus":
             # concatenar e printar numa linha unica
-            txt += " bus: " + bus + " personNumber: " + str(traci.vehicle.getPersonNumber(bus))
-           
+            txt += " bus: " + bus + " personNumber: " + \
+                str(traci.vehicle.getPersonNumber(bus))
+
     print(txt)
-        
-    
+
+
+def print_busStopPersons(orderBusStops, step):
+    txt = "step: " + str(step)
+    for i in orderBusStops:
+        txt += " busStopName: " + \
+                str(traci.busstop.getName(
+                    i[0])) + " personCount: " + str(traci.busstop.getPersonCount(i[0]))
+        print(txt)
 
 if __name__ == "__main__":
-        
+
     options = get_options()
 
     if options.nogui:
@@ -65,35 +73,22 @@ if __name__ == "__main__":
         sumoBinary = checkBinary('sumo-gui')
 
     sumoCmd = [sumoBinary, "-c", "osm.sumocfg"]
-    
+
     traci.start(sumoCmd)
-    
+
     busstops = traci.busstop.getIDList()
-    
+
     orderBusStops = orderBusStopByName(busstops)
-        
+
     step = 0
     while step <= 1000:
         traci.simulationStep()
-        
-        # for i in orderBusStops:
-           
-        #     # if(traci.busstop.getPersonCount(i[0])>0):
-        #         print(
-        #             "step: " + str(step) + "    " +
-        #             "busStopName: " + str(traci.busstop.getName(i[0])) + "    " +
-        #             "busStopId: " + i[0] + "    " +
-        #             "countPersons: " + str(traci.busstop.getPersonCount(i[0])) + "    " +
-        #             ""
-        #             )
-                
-        if (step % 50 == 0):    
-            getBus(step)
-                 
+
+
+        if (step % 50 == 0):
+            # print_busStopPersons(orderBusStops, step)
+            print_bus_persons(step)
+
         step += 1
-        
+
     traci.close()
-
-
-    
-     
