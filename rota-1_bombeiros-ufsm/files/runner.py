2681bf60cf61d42b172cfc3f3ec39b38a58978d4
alisson.sganzerla <alisson.sganzerla@gmailcom>
2021-11-18 09:12:27 -0300

    chore: gerando relatorio csv

--- rota-1_bombeiros-ufsm/files/runner.py
+++ rota-1_bombeiros-ufsm/files/runner.py
@@ -1,9 +1,9 @@
 from __future__ import absolute_import
 from __future__ import print_function
 
+import optparse
 import os
 import sys
-import optparse
 
 # we need to import python modules from the $SUMO_HOME/tools directory
 if 'SUMO_HOME' in os.environ:
@@ -17,60 +17,59 @@ from sumolib import checkBinary  # noqa
 import traci
 
 
-def noGUI():
-    # define se exibe interface grafica ou nao do SUMO
+def no_gui():
     return True
 
 
 def get_options():
-    optParser = optparse.OptionParser()
-    optParser.add_option("--nogui", action="store_true",
-                         default=noGUI(), help="run the commandline version of sumo")
-    options, args = optParser.parse_args()
+    opt_parser = optparse.OptionParser()
+    opt_parser.add_option("--nogui", action="store_true",
+                          default=no_gui(), help="run the commandline version of sumo")
+    options, args = opt_parser.parse_args()
     return options
 
-def orderBusStopByName(busstops):
-    row = len(busstops)
+
+def order_bus_stop_by_name(bus_stop_id):
+    row = len(bus_stop_id)
     col = 2
-    orderBusStops = [[0 for j in range(col)] for i in range(row)]
+    order_bus_stops = [[0 for j in range(col)] for i in range(row)]
 
     i = 0
-    for bus in busstops:
-        orderBusStops[i][0] = bus
-        orderBusStops[i][1] = traci.busstop.getName(bus).split("p")[1]
+    for bus in bus_stop_id:
+        order_bus_stops[i][0] = bus
+        order_bus_stops[i][1] = traci.busstop.getName(bus).split("p")[1]
         i += 1
 
     # ordenar as paradas pelo nome
-    orderBusStops.sort(key=lambda x: (int(x[1]), int(x[0])))
+    order_bus_stops.sort(key=lambda x: (int(x[1]), int(x[0])))
 
-    return orderBusStops
+    return order_bus_stops
 
 
-def print_bus_persons(step, busSpeed):
-    allBus = traci.vehicle.getIDList()
-    txt = "bus " + str(step)
-    for bus in allBus:
+def print_persons_in_bus(step_value, bus_speed):
+    all_bus = traci.vehicle.getIDList()
+    txt = str(step_value)
+    for bus in all_bus:
         if traci.vehicle.getTypeID(bus) == "bus":
-            if (bus == "flow_bombeiros-ufsm.2" or bus == "flow_bombeiros-ufsm.3" or bus == "flow_bombeiros-ufsm.4"):
-                traci.vehicle.setSpeed(bus, busSpeed)
+            if bus == "flow_bombeiros-ufsm.2" or bus == "flow_bombeiros-ufsm.3" or bus == "flow_bombeiros-ufsm.4":
+                traci.vehicle.setSpeed(bus, bus_speed)
 
-             # concatenar e printar numa linha unica
-        txt += " " + bus + " : " + \
-            str(traci.vehicle.getPersonNumber(bus))
+        txt += ", " + bus + ", " + \
+               str(traci.vehicle.getPersonNumber(bus))
 
-    print(txt)
+    return txt + "\n"
 
 
-def print_busStopPersons(orderBusStops, step):
-    txt = "busStops " + str(step)
+def print_persons_in_bus_stop(step_value):
+    txt = "busStops " + str(step_value)
     aux = 0
-    for i in orderBusStops:
-        if aux >= 5 and aux <= 30:
+    for i in ordered_bus_stops:
+        if 5 <= aux <= 30:
             # txt += " busStopName: " + \
             txt += " " + \
-                str(traci.busstop.getName(
-                    # i[0])) + " personCount: " + str(traci.busstop.getPersonCount(i[0]))
-                    i[0])) + " : " + str(traci.busstop.getPersonCount(i[0]))
+                   str(traci.busstop.getName(
+                       # i[0])) + " personCount: " + str(traci.busstop.getPersonCount(i[0]))
+                       i[0])) + " : " + str(traci.busstop.getPersonCount(i[0]))
         aux += 1
 
     print(txt)
@@ -85,35 +84,40 @@ if __name__ == "__main__":
     else:
         sumoBinary = checkBinary('sumo-gui')
 
-    sumoCmd = [sumoBinary, "-c", "osm.sumocfg"]
+    sumo_cmd = [sumoBinary, "-c", "osm.sumocfg"]
 
-    traci.start(sumoCmd)
+    traci.start(sumo_cmd)
 
-    busstops = traci.busstop.getIDList()
+    bus_stops = traci.busstop.getIDList()
 
-    orderBusStops = orderBusStopByName(busstops)
+    ordered_bus_stops = order_bus_stop_by_name(bus_stops)
 
     # traci.trafficlight.setPhaseDuration("GS_637770324", 200)
 
+    newFile = open("bus_persons20.csv", "w")
+    newFile.write("step, bus_id, count_people \n")
     step = 0
     while step <= 2000:
         traci.simulationStep()
 
-        if (step % 50 == 0):
-            # print_busStopPersons(orderBusStops, step)
-            print_bus_persons(step, 20.0)
+        if step % 50 == 0:
+            newFile.write(print_persons_in_bus(step, 20.0))
 
         step += 1
-        
+
+    newFile.close()
+
+    newFile = open("bus_persons80.csv", "w")
+    newFile.write("step, bus_id, count_people \n")
     step = 0
-    
     while step <= 2000:
         traci.simulationStep()
 
-        if (step % 50 == 0):
-            # print_busStopPersons(orderBusStops, step)
-            print_bus_persons(step, 80.0)
+        if step % 50 == 0:
+            newFile.write(print_persons_in_bus(step, 80.0))
 
         step += 1
+
+    newFile.close()
 
     traci.close()
