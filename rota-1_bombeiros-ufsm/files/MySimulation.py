775756aabf967db2c27a5c62da5e19674e1a6187
alisson.sganzerla <alisson.sganzerla@gmailcom>
2021-11-25 22:41:23 -0300

    chore: filter once buses

--- rota-1_bombeiros-ufsm/files/MySimulation.py
+++ rota-1_bombeiros-ufsm/files/MySimulation.py
@@ -5,14 +5,16 @@ class MySimulation:
         self.all_bus_stops = self.get_all_bus_stops()
         self.all_buses_simulation = []
         self.all_buses_circulation = []
+        self.all_people_on_bus = []
 
     def get_all_bus_stops(self):
         all_bus_stop = self.traci.busstop.getIDList()
         return self._sort_bus_stop_by_name(all_bus_stop)
 
     def get_all_bus(self):
-        all_bus = self.traci.vehicle.getIDList()
-        return self._sort_bus_by_name(all_bus)
+        all_vehicles = self.traci.vehicle.getIDList()
+        return self._sort_bus_by_name(self._filter_once_buses(all_vehicles))
+
 
     # metodos privados
 
@@ -31,13 +33,13 @@ class MySimulation:
 
         return array_2d
 
-    def _sort_bus_by_name(self, bus_ids):
-        row = len(bus_ids)
+    def _sort_bus_by_name(self, vehicle_ids):
+        row = len(vehicle_ids)
         col = 2
         array_2d = [[0 for j in range(col)] for i in range(row)]
 
         i = 0
-        for bus in bus_ids:
+        for bus in vehicle_ids:
             array_2d[i][0] = bus
             array_2d[i][1] = bus.split(".")[1]
             i += 1
@@ -45,3 +47,10 @@ class MySimulation:
         array_2d.sort(key=lambda x: (int(x[1]), (x[0])))
 
         return array_2d
+
+    def _filter_once_buses(self, vehicle_ids):
+        filtered = []
+        for v in vehicle_ids:
+            if (self.traci.vehicle.getTypeID(v) == "bus"):
+                filtered.append(v)
+        return filtered
